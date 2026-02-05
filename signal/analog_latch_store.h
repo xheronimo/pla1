a76da8f138e4978f846e3e8e70a03432b0d78d32
xheronimo <javi_direc@hotmail.com>
2026-02-06 00:41:49 +0100

    Delete signal directory

--- signal/analog_latch_store.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-bool loadAnalogLatch(const String& id, float& value);
-void saveAnalogLatch(const String& id, float value);
--- signal/digital_latch_store.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-bool loadLatch(const String& id);
-void saveLatch(const String& id, bool value);
-void clearAllLatches();
